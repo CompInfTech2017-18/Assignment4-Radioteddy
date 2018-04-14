b10755a34f480e43a615198c4a9301f28fe30822
Radioteddy <f.o.akhmetov@gmail.com>
2018-04-14 23:19:11 +0300

    last changing

--- QC1.py
+++ QC1.py
@@ -8,19 +8,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import scipy.integrate as integrate
+import warnings
+warnings.filterwarnings('ignore')
+np.set_printoptions(threshold=np.nan)
 
+#initial data
 Ox = 20
 Nx = 500
 T = 1000
 dt = 0.1
-xc = 3.
-h = 3.
-
-def norma(psi, xarr):
-    return integrate.simps(psi*np.conj(psi), xarr)
-
-def Psi0(x, xc, sigma):
-    return np.exp(-0.5*(x+xc)**2/sigma**2)/(sigma*np.sqrt(2*np.pi))
+h = 5. #width of potential
+x0 = 0 #center of gap
+d = 2. #0.5 width of gap
+xc = x0-d-h/2 #center left of potential
 
 Psi = np.zeros((Nx, T), dtype=np.complex128) #matrix of wave-function
 W = np.zeros((Nx, T), dtype=np.complex128) #matrix of probability
@@ -31,15 +31,31 @@ dx = np.fabs(xarr[0]-xarr[1])
 eta = dt/dx**2
 print(eta)
 
+#function of potential
+def U(x, x0, h, d, U0):
+    if x>=x0-d-h and x<=x0-d:
+        return U0
+    elif x>=x0+d and x<=x0+d+h:
+        return U0
+    else:
+        return 0
 
+#function of normalization
+def norma(psi, xarr):
+    return integrate.simps(psi*np.conj(psi), xarr)
+
+#initial WF of particle
+def Psi0(x, xc, sigma):
+    return np.exp(-0.5*(x-xc)**2/sigma**2)/(sigma*np.sqrt(2*np.pi))
+
+#initial distribution of WF
 Psi[:,0] = Psi0(xarr, xc, 0.5*h)
 Psi[:,0] = Psi[:,0]/np.sqrt(norma(Psi[:,0], xarr))
 W[:,0] = Psi[:,0]*np.conj(Psi[:,0])
-U0 = -2.*np.amax(Psi[:,0]**2).astype(float)
-Um[(xarr >= -xc - 0.6*h)*(xarr <= -xc + 0.6*h)] = U0
-Um[(xarr >= xc - 0.6*h)*(xarr <= xc + 0.6*h)] = U0
-
-
+U0 = -2.*np.amax(Psi[:,0]*np.conj(Psi[:,0])).astype(float)
+for i in range(Nx):
+    x = xarr[i]
+    Um[i] = U(x, x0, h, d, U0)
 
 
 #first step - explicit scheme
@@ -68,6 +84,7 @@ def updatefig(i):
    line = ax.plot(xarr, Wt, color='blue', linewidth=1.0)
    return line,
 
-ani = animation.FuncAnimation(fig, updatefig, np.arange(0, T), interval=0)
+ani = animation.FuncAnimation(fig, updatefig, np.arange(0, T), interval=1)
+ani.save('QC.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
 
 plt.show()
